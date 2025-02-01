8abaae05e86dd920e4fd5a5363fcb9ad112f4821
James <jameshenriquesp019@gmail.com>
2025-02-01 15:23:03 -0300

    Organização de imports
    Import não utilizado removido

--- src/main/java/br/com/jnb/productsapi/controller/ProdutoController.java
+++ src/main/java/br/com/jnb/productsapi/controller/ProdutoController.java
@@ -5,7 +5,6 @@ import br.com.jnb.productsapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
