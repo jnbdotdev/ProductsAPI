ac32b4f758812415036e2e5ba3df8c9d25bf4b98
James <jameshenriquesp019@gmail.com>
2025-02-01 13:33:34 -0300

    Busca por parâmetro
    Adicionada busca por parâmetro para que seja possível buscar por nome por exemplo.

--- src/main/java/br/com/jnb/productsapi/controller/ProdutoController.java
+++ src/main/java/br/com/jnb/productsapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import br.com.jnb.productsapi.model.Produto;
 import br.com.jnb.productsapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -43,5 +44,10 @@ public class ProdutoController {
 
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
     }
 }
